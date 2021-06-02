e4a7512839f4c4d693beda805f8e2d9bc5c68e85
Yuna <ilena@f4d3z.space>
2021-06-02 14:01:13 +0100

    Update build.sh
    now removes unnecessary files left over from compiling

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-python3 -m eel Installer.py web --onefile --noconsole 
+python3 -m eel Installer.py web --onefile --noconsole
+rm -rf Installer.spec
+rm -rf __pycache__
+rm -rf build
+cp dist/Installer Installer
+rm -rf dist
